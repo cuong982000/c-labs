5a357dc1b6d75a3e099c9b9d1ed2a5e0c439c84b
cuong982000 <59680520+cuong982000@users.noreply.github.com>
2020-02-18 12:33:47 +0700

    Add files via upload

---
+++ CSP0005.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+int main()
+{
+    int i,number;
+    char choice;
+    do{
+    printf("\nPlease enter a number (1-9): "); 
+	scanf("%d",&number);
+    while(number < 1 || number > 9)
+    {
+        printf("Please enter a number (1-9): "); 
+		scanf("%d",&number);
+    }
+    printf("Multiplication table for %d number\n",number);
+    for(i=0;i<=10;i++)
+    {
+        printf("%d X %d = %d\n",number,i,i*number);
+    }
+    printf("\nPress enter to continue another reverse, ESC to exit ");
+    choice = getch();
+    }while(choice != 27);
+    return 0;
+}
